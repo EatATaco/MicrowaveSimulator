60b8c802cf6d0f518b4a911c1cbafea0101b41f8
Charles Barnes <cbarnes@digitalgridinc.com>
2016-05-17 11:31:26 -0500

    temp push of EVERYTHING

---
+++ app/src/main/java/com/holychicken/microwavesimulator/SevenSegmentClock.java
@@ -0,0 +1,14 @@
+package com.holychicken.microwavesimulator;
+
+import android.widget.TextClock;
+
+/**
+ * Created by Charles on 5/15/2016.
+ */
+public class SevenSegmentClock {
+    public SevenSegmentClock(){
+
+    }
+
+    private TextClock internalClock = new TextClock
+}
